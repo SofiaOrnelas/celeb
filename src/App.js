7bb1e7f1dd439b11003afc37ffc77c1eafcbd2bc
Sofia <ascto1@gmail.com>
2019-05-14 12:57:41 +0100

    It2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+ 
+ class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      celebrities: [],  
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState(
+      this.state.celebrities.name,
+      this.state.celebrities.known_for
+    )
+  }
+   render() {
+     return (
+      
+       <div className="Celebrities">
+         <h1>Movie celebrities </h1>
+         {this.state.celebrities.map(celebrities => <ul key={celebrities.id}> <img src={"https://image.tmdb.org/t/p/w185"+celebrities.profile_path} alt="" className="picture" onClick={this.handleClick}/> {} {celebrities.name}</ul>)}
+       </div>
+     )
+   }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+   componentDidMount(){
+    axios.get('https://api.themoviedb.org/3/person/popular?page=6&api_key=e17acce98195ad86d300350e69858f6f')
+    .then(celebrities => {
+      console.log(celebrities.data.results)
+      this.setState({
+        celebrities:celebrities.data.results
+      })
+    })
+  }
+ }
+ 
+ 
 export default App;
